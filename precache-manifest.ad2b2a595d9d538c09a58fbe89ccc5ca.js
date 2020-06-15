0fff8d89385f990dd2c5a6a0234511511bf192f2
panweizheng <c.wzpan@gmail.com>
2020-06-15 09:29:55 +0800

    commit

---
+++ precache-manifest.ad2b2a595d9d538c09a58fbe89ccc5ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47b23de4a8a78711da649557a2465e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e22af29f8812118daf6a",
+    "url": "/static/css/2.36fcf5ee.chunk.css"
+  },
+  {
+    "revision": "e47994beae381a96a116",
+    "url": "/static/css/main.538285c4.chunk.css"
+  },
+  {
+    "revision": "e22af29f8812118daf6a",
+    "url": "/static/js/2.4edbeab5.chunk.js"
+  },
+  {
+    "revision": "83584953bf4953ce944e6f1e8fc4af0b",
+    "url": "/static/js/2.4edbeab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47994beae381a96a116",
+    "url": "/static/js/main.7b6a177e.chunk.js"
+  },
+  {
+    "revision": "bee7754fd75af9663bac",
+    "url": "/static/js/runtime-main.23fe0c48.js"
+  },
+  {
+    "revision": "2b9b4872cd25494093c1eb14f0264a0b",
+    "url": "/static/media/jsoneditor-icons.2b9b4872.svg"
+  }
+]);
